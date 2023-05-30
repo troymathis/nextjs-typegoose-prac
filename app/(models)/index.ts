1eb729d39d3a1280ab54e39c452e2afd52f2e267
Troy Mathis <troy.mathis00@gmail.com>
2023-05-29 22:16:26 -0500

    created model from todo class using typegoose

--- app/(models)/index.ts
+++ app/(models)/index.ts
@@ -1,6 +1,4 @@
-// models/index.ts
 import { Todo } from "../(models)/Todo";
 import { getModelForClass } from "@typegoose/typegoose";
 
 export const TodoModel = getModelForClass(Todo);
-// add other models here
